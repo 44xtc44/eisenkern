f5ef738ef6fdb01b8c0b4d1f8a52964545297fea
4xtc <44xtc44@gmail.com>
2023-04-03 00:10:15 +0200

    replaced name of constants to match pkg

--- eisenmp/utils/eisenmp_constants.py
+++ eisenmp/utils/eisenmp_constants.py
@@ -1,15 +1,14 @@
-""" eisenmp package constants definition, can override """
+""" eisenmp package constants definition,
+override !
+"""
 
-# Package version
-VERSION = '0.1.0'
-# Generator part
-HEADER_MSG = 'GHETTO_BOSS'
-PERF_HEADER_ETA = 'PROC_PERF_ETA_'
+GOT_RESULT_OUTPUT_Q = 'GOT_RESULT_OUTPUT_Q'  # append line to result list and check, already red msg
+GOT_RESULT_RESULT_THREAD = 'RESULT_INTERIM_GOT'  # append line to result list and check
+HEADER_MSG = 'EISENMP_BOSS'
 NUM_ROWS = 10_000
+PERF_HEADER_ETA = 'PERF_HEADER_ETA_'
 STOP_MSG = 'STOP'
 STOP_PROCESS = 'STOP_PROCESS'
 STOP_CONFIRM = 'PROC_STOP_'
-GOT_RESULT_OUTPUT_Q = 'GHETTO_GANG_GOT'  # append line to result list and check, already red
-GOT_RESULT_RESULT_THREAD = 'RESULT_INTERIM_GOT'  # append line to result list and check
-RESULT_HEADER_PROC = 'PROC_RESULT_'
-# Worker part
+RESULT_HEADER_PROC = 'RESULT_HEADER_PROC_'
+
