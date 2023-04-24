ad71887a36acff18c89f00359baff44cc43c70d0
4xtc <44xtc44@gmail.com>
2023-04-24 10:25:37 +0200

    update doc

--- eisenmp/__init__.py
+++ eisenmp/__init__.py
@@ -17,8 +17,6 @@ An example function cracks a game quest.
 import time
 import threading
 
-import eisenmp.eisenmp_q_coll as coll
-import eisenmp.eisenmp_procenv as procenv
 import eisenmp.utils.eisenmp_utils as e_utils
 import eisenmp.utils.eisenmp_constants as const
 from eisenmp.eisenmp_q_coll import QueueCollect
@@ -70,20 +68,14 @@ class Mp(QueueCollect):
         generator = kw['generator']  # no generator for run_q_feeder, crash for sure
         rows_max = kw['ROWS_MAX'] if 'ROWS_MAX' in kw and kw['ROWS_MAX'] else const.ROWS_MAX  # processor workload
         feeder_input_q = kw['input_q'] if 'input_q' in kw else self.mp_input_q  # use default if not specified
-
-        result_key = ''  # key name where Queue is stored as value, need a name to distinguish results in result dict
-        for tup in self.q_name_id_lst:
-            name, q_id, _ = tup  # name id q_ref
-            if q_id == id(feeder_input_q):  # unique Python id for objects
-                result_key = name
-                break
+        q_name = q_name_get(self.q_name_id_lst, feeder_input_q)
 
         start = time.perf_counter()
         num_gen = e_utils.consecutive_number()
         while 1:
             if self.all_threads_stop:
                 break
-            chunk_lst = create_transport_ticket(num_gen, result_key)
+            chunk_lst = create_transport_header(num_gen, q_name)
             for _ in range(rows_max):
                 try:
                     chunk_lst.append(next(generator))
@@ -105,10 +97,22 @@ class Mp(QueueCollect):
                 break
 
 
-def create_transport_ticket(num_gen, result_key):
+def q_name_get(q_name_id_lst, feeder_input_q):
+    """
+    """
+    q_name = ''  # result can be stored as dict[q_name] = {_TID_1: foo, _TID_2: bar}
+    for tup in q_name_id_lst:
+        name, q_id, _ = tup  # name id q_ref
+        if q_id == id(feeder_input_q):  # find feeder_input_q in q_name_id_lst, unique Python id() for objects
+            q_name = name
+            break
+    if not q_name:
+        q_name = 'mp_input_q'
+    return q_name
+
+
+def create_transport_header(num_gen, q_name):
     """Semicolon to split easy.
     """
-    if not result_key:
-        result_key = 'mp_input_q'
-    serial_num = ';' + const.TICKET_ID_PREFIX + f'{str(next(num_gen))};'  # ';_TID_1;'
-    return [result_key + serial_num]
+    ticket = q_name + ';' + const.TICKET_ID_PREFIX + f'{str(next(num_gen))};'  # ';_TID_1;'
+    return [ticket]
